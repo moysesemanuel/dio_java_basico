65283b639545a301da39b7e80a595e5e0bc37ff4
moysescosta <mecs.cwb@gmail.com>
2024-12-24 11:49:20 -0300

    comentando exemplos

--- java_anatomia_classes/src/MinhaClasse.java
+++ java_anatomia_classes/src/MinhaClasse.java
@@ -15,17 +15,17 @@ public static void main(String [] args) {
  
     // Declarações inválidas de variáveis:
 
-    int numero&um = 1; // Os únicos símbolos permitidos são _ e $.
-    int 1numero = 1; // Uma variável não pode começar com númeral.
-    int numero um = 1; // Não pode ter espaço no nome da variável.
-    int long = 1; // long faz parte das palavras reservadas da linguagem.
+    // int numero&um = 1; // Os únicos símbolos permitidos são _ e $.
+    // int 1numero = 1; // Uma variável não pode começar com númeral.
+    // int numero um = 1; // Não pode ter espaço no nome da variável.
+    // int long = 1; // long faz parte das palavras reservadas da linguagem.
 
     // Declarações válidas de variáveis:
 
-    int numero$um = 1;
-    int numero1 = 1;
-    int numeroUm = 1;
-    int longo = 1;
+    // int numero$um = 1;
+    // int numero1 = 1;
+    // int numeroUm = 1;
+    // int longo = 1;
 
 }
 
