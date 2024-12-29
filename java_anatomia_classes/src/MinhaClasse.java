c5df0c8ea8af01add2205898603fcd3be01e85d6
moysescosta <mecs.cwb@gmail.com>
2024-12-29 13:41:28 -0300

    Alterei a forma de comentário

--- java_anatomia_classes/src/MinhaClasse.java
+++ java_anatomia_classes/src/MinhaClasse.java
@@ -11,21 +11,21 @@ public static void main(String [] args) {
     int ESTADOS_BRASILEIRO = 27;
     int ANO_2000 = 2000;
 
-    System.out.println(BR + "\n" + PI + "\n" + ESTADOS_BRASILEIRO + "\n" + ANO_2000);
+    System.out.print(BR + "\n" + PI + "\n" + ESTADOS_BRASILEIRO + "\n" + ANO_2000);
  
-    // Declarações inválidas de variáveis:
+    /*Declarações inválidas de variáveis:
 
-    // int numero&um = 1; // Os únicos símbolos permitidos são _ e $.
-    // int 1numero = 1; // Uma variável não pode começar com númeral.
-    // int numero um = 1; // Não pode ter espaço no nome da variável.
-    // int long = 1; // long faz parte das palavras reservadas da linguagem.
+    int numero&um = 1; // Os únicos símbolos permitidos são _ e $.
+    int 1numero = 1; // Uma variável não pode começar com númeral.
+    int numero um = 1; // Não pode ter espaço no nome da variável.
+    int long = 1; // long faz parte das palavras reservadas da linguagem.
 
-    // Declarações válidas de variáveis:
+    Declarações válidas de variáveis:
 
-    // int numero$um = 1;
-    // int numero1 = 1;
-    // int numeroUm = 1;
-    // int longo = 1;
+    int numero$um = 1;
+    int numero1 = 1;
+    int numeroUm = 1;
+    int longo = 1;*/
 
 }
 
